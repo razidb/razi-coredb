228c7808d749fadee3c8ed805adf37e7a781e690
mahdyhamad <mahdy.hamad45@gmail.com>
2022-12-02 21:52:00 +0300

    fix  query

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,13 +3,24 @@ package org.example;
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import auth.AuthManager;
 import auth.AuthController;
 import controller.DBController;
+import org.json.simple.JSONArray;
 
 
 public class Server{
+    /*
+    *
+    * Main database access point, responsible for receiving initial
+    * clients connections.
+    * Making sure data is loaded initially when the server is initiated.
+    *
+    * */
+
     // leave port number > 1023 as linux reserve ports less than 1023
     private static final int PORT = 4999;
     private static ServerSocket server;
@@ -17,16 +28,34 @@ public class Server{
     public static DBController dbController;
     public static AuthController authController;
 
+    private static Logger logger;
+
     public static void main(String[] args) throws IOException {
         // load db and auth managers on server initialized
         authManager = AuthManager.getAuthManager();
         dbController = DBController.getDbController();
         authController = AuthController.getAuthController();
+        logger = Logger.getLogger(Server.class.getName());
+        /*
+        // Adding 1 million records in the database
+        JSONArray data = new JSONArray();
+        for (int i=1; i< 1000*1000; i++){
+            JSONObject object = new JSONObject();
+            object.put("_id", UUID.randomUUID().toString());
+            object.put("firstName", "test" + i);
+            object.put("lastName", "test" + i);
+            object.put("username", "test" + i);
+            object.put("status", i%2 == 0?"A":"I");
+            data.add(object);
+        }
+
+        dbController.getDatabase("test").getCollection("test").getController().addDocumentsToCollection(data);
+        */
 
         server = new ServerSocket(PORT);
 
         while (true){
-            System.out.println("Waiting for a client to connect....");
+            logger.log(Level.INFO, "Waiting for a client to connect....");
             handleConnection();
         }
     }
@@ -38,14 +67,13 @@ public class Server{
             thread.start();
         }
         catch (IOException e){
-            System.out.println("Could not listen on port: " + PORT);
-            System.out.println(e.getMessage());
+            logger.warning("Could not listen on port: " + PORT);
+            logger.warning(e.getMessage());
             System.exit(-1);
         }
         catch (Exception syntaxException){
-            System.out.println("Invalid syntax: " + syntaxException.getMessage());
+            logger.warning("Invalid syntax: " + syntaxException.getMessage());
             System.out.println(Arrays.toString(syntaxException.getStackTrace()));
-
         }
     }
 }
